0c6b23ac8f0a0818360bd650d4236ad8a7d3b135
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-04-08 16:30:10 +0900

    파일 분리 & 놀이학습 접속까지 구현

---
+++ CSER/play.php
@@ -0,0 +1,20 @@
+<?php
+require_once("index_top.php");
+?>
+<title>놀이학습</title>
+	<style>
+		@import url("css/index.css");
+	</style>
+</head>
+<?php
+require_once("index_bottom.php");
+?>
+<section>
+	<article>
+		<div class="box" style="background: #BDBDBD;">
+		    <img class="profile" src="/images/tistory.JPG">
+		</div>
+	</article>
+</section>
+</body>
+</html>
